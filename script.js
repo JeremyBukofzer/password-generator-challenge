6590c059cb753d989b3b8555d39fbbec31bb907e
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-09-17 11:09:53 -0500

    added alert for password length

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"//all possible charcters in password
+  var criteriaLength = prompt("Please select password length (can be 8-128 characters:");
+
+  if (!criteriaLength) {
+    alert("Please enter a value.")
+  } else if (criteriaLength < 8 || criteriaLength > 128) {
+    alert("Please enter number 8-128.")
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
