86182d0b246c61e1bd6517ac19c00eaaa4fbb39c
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-09-17 11:45:10 -0500

    adds criteria selectors

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+var criteriaLength;
+var criteriaLower;
+var criteriaUpper;
+var criteriaNumber;
+var criteriaSpecial;
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var criteriaLength = prompt("Please select password length (can be 8-128 characters:");
+  // Prompt to enter password length
+  criteriaLength = prompt("Please select password length (can be 8-128 characters:");
 
+  // Alerts if password length value is not equal to criteria
   if (!criteriaLength) {
-    alert("Please enter a value.")
+    alert("Please enter a number value.");
+    writePassword();
+ 
   } else if (criteriaLength < 8 || criteriaLength > 128) {
-    alert("Please enter number 8-128.")
+    alert("Please enter number 8-128.");
+    writePassword();
+ 
+  } else {
+    // Options to include criteria
+    criteriaLower = confirm("would you like to include lowercase letters?")
+    criteriaUpper = confirm("would you like to include uppercase letters?")
+    criteriaNumber = confirm("would you like to include numbers?")
+    criteriaSpecial = confirm("would you like to include special characters?")
   }
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
