6fe8dd67ec326e5ff9b0a31945cd18de05044437
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-09-18 15:56:11 -0500

    Cleaned up arrays, added some if statements

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-var criteriaLength;
-var criteriaLower;
-var criteriaUpper;
-var criteriaNumber;
-var criteriaSpecial;
 
-lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "?"];
+var charsLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charsUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var charsNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var charsSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "?"];
+var selections = [];
 
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
+function generatePassword() {
   // Prompt to enter password length
   criteriaLength = prompt("Please select password length (can be 8-128 characters:");
 
   // Alerts if password length value is not equal to criteria
   if (!criteriaLength) {
-    alert("Please enter a number value.");
-    writePassword();
  
   } else if (criteriaLength < 8 || criteriaLength > 128) {
     alert("Please enter number 8-128.");
@@ -36,14 +24,35 @@ function writePassword() {
     criteriaSpecial = confirm("would you like to include special characters?")
   };
 
-  if (criteriaLower && criteriaUpper && criteriaNumber)
-  
+  if (criteriaLower) {
+    selections = selections.concat(charsLower)
+  }
+  if (criteriaUpper) {
+    selections = selections.concat(charsUpper)
+  }
+  if (criteriaNumber) {
+    selections = selections.concat(charsNumber)
+  }
+  if (criteriaSpecial) {
+    selections = selections.concat(charsSpecial)
+  }
+
+  }
+};
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
+  
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
